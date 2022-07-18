098caaa17d997bd44eb0d854f6f1f3fd1f628550
Nazanin-Rezaei <73320246+Nazanin-Rezaei@users.noreply.github.com>
2022-07-18 10:24:26 -0400

    Add files via upload

---
+++ client/my-app/src/App.js
@@ -0,0 +1,27 @@
+
+import './App.css';
+import Animated from './eth-animation.svg'
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+ <object type="image/svg+xml" data={Animated}></object>
+  
+
+        {/* <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a> */}
+      </header>
+    </div>
+  );
+}
+
+export default App;
